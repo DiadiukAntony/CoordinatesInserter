c7bf60f66cdd61dbc87327b2d462ca6a5809acc8
Anton Diadiuk <antond@lyft.com>
2024-02-20 21:25:43 +0300

    Func operates on a real coordinates. It advance them in any (positive / negative) directions based on Start & Finish input

--- CoordinatesInserter.playground/Contents.swift
+++ CoordinatesInserter.playground/Contents.swift
@@ -1,15 +1,4 @@
 // Creator Anton Diadiuk. Mysqadiam@gmail.com
-// This is a simple approach, that doesn't use Srtide function and strictly get only CLLocationCoordinate2D value types.
-
-// TODO
-// 1. Adjust getCoordinatesList to increment Negative values (as coordinates could be negative). Now it works with only with >0.
-// 2. Continue on extension of this feature to get more than 2 points. Func should operate with more input coordinates.
-// 3. Export this Playground to a Console app -> Move functions / values / tests in a separate files.
-// 4. Add "import from file" function to pass .gpx files to this Console app.
-// 5. Add more tests.
-// 6. Test and release :)
-
-
 
 import CoreLocation
 import XCTest
@@ -19,63 +8,81 @@ import PlaygroundSupport
 // This is for In-playground XCTestCases only.
 PlaygroundPage.current.needsIndefiniteExecution = true
 
-let start = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
-let finish = CLLocationCoordinate2D(latitude: 10.0, longitude: 0.0)
+let startWpt = CLLocationCoordinate2D(latitude: -10.0, longitude: 0.0)
+let finishWpt = CLLocationCoordinate2D(latitude: -20.0, longitude: 10.0)
 let step = CLLocationCoordinate2D(latitude: 1.0, longitude: 1.0)
 
-extension CLLocationCoordinate2D: Equatable {
+extension CLLocationCoordinate2D {
     public static func == (leftHandEquitable: CLLocationCoordinate2D, rightHandEquitable: CLLocationCoordinate2D) -> Bool {
         return leftHandEquitable.latitude == rightHandEquitable.latitude && leftHandEquitable.longitude == rightHandEquitable.longitude
     }
+
+    var isLatPositive: Bool {
+        return latitude >= 0
+    }
+
+    var isLongPositive: Bool {
+        return longitude < 0
+    }
 }
 
-func getCoordinatesList(_ start: CLLocationCoordinate2D, _ finish: CLLocationCoordinate2D, _ step: CLLocationCoordinate2D) -> [CLLocationCoordinate2D] {
+var coordinates = Array<Any>()
+coordinates = getCoordinatesList(startWpt, finishWpt, step)
+
+func getCoordinatesList(_ startWpt: CLLocationCoordinate2D, _ finishWpt: CLLocationCoordinate2D, _ step: CLLocationCoordinate2D) -> [CLLocationCoordinate2D] {
     var coordinates = [CLLocationCoordinate2D]()
-    var current = start
+    var currentWpt = startWpt
 
-    let didLatChange = abs(start.latitude - finish.latitude)
-    let didLongChange = abs(start.longitude - finish.longitude)
+    let latChanged = startWpt.latitude != finishWpt.latitude
+    let longChanged = startWpt.longitude != finishWpt.longitude
 
-    if (didLatChange != 0 && didLongChange == 0) {
-        while current.latitude <= finish.latitude {
-            coordinates.append(current)
-            current.latitude = current.latitude + step.latitude
+    while currentWpt.latitude != finishWpt.latitude || currentWpt.longitude != finishWpt.longitude {
+        coordinates.append(currentWpt)
+
+        if latChanged {
+            if (currentWpt.isLatPositive && finishWpt.isLatPositive && currentWpt.latitude < finishWpt.latitude) ||
+                (!currentWpt.isLatPositive && !finishWpt.isLatPositive && currentWpt.latitude < finishWpt.latitude) {
+                currentWpt.latitude += step.latitude
+            } else if (currentWpt.isLatPositive && finishWpt.isLatPositive && currentWpt.latitude > finishWpt.latitude) ||
+                (!currentWpt.isLatPositive && !finishWpt.isLatPositive && currentWpt.latitude > finishWpt.latitude) {
+                currentWpt.latitude -= step.latitude
+            }
         }
-    } else if (didLatChange == 0 && didLongChange != 0) {
-        while current.longitude <= finish.longitude {
-            coordinates.append(current)
-            current.longitude = current.longitude + step.longitude
-        }
-    } else {
-        while current.latitude <= finish.latitude && current.longitude <= finish.longitude {
-            coordinates.append(current)
-            current.latitude = current.latitude + step.latitude
-            current.longitude = current.longitude + step.longitude
+
+        if longChanged {
+            if (currentWpt.isLongPositive && finishWpt.isLongPositive && currentWpt.longitude < finishWpt.longitude) ||
+                (!currentWpt.isLongPositive && !finishWpt.isLongPositive && currentWpt.longitude < finishWpt.longitude) {
+                currentWpt.longitude += step.longitude
+            } else if (currentWpt.isLongPositive && finishWpt.isLongPositive && currentWpt.latitude > finishWpt.latitude) ||
+                (!currentWpt.isLongPositive && !finishWpt.isLongPositive && currentWpt.longitude > finishWpt.longitude) {
+                currentWpt.longitude -= step.longitude
+            }
         }
     }
+    coordinates.append(finishWpt)
+    print(coordinates)
     return coordinates
 }
 
-print(getCoordinatesList(start, finish, step))
-
 //__________________________________________________________________________________________
 
-class CoordinateGeneratorTests: XCTestCase {
+//class CoordinateGeneratorTests: XCTestCase {
+//
+//    func testStartFinishCoordinates() {
+//        let start = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
+//        let finish = CLLocationCoordinate2D(latitude: 10.0, longitude: 0.0)
+//        let step = CLLocationCoordinate2D(latitude: 1.0, longitude: 1.0)
+//
+//        let coordinates = getCoordinatesList(start, finish, step)
+//
+//        if coordinates.count > 0 {
+//            XCTAssertEqual(coordinates[0], start, "List starts with a proper coordinate")
+//        } else {
+//            XCTFail("No coordinates generated")
+//        }
+//    }
+//}
+//
+//let testSuite = CoordinateGeneratorTests.defaultTestSuite
+//testSuite.run()
 
-    func testStartFinishCoordinates() {
-        let start = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
-        let finish = CLLocationCoordinate2D(latitude: 10.0, longitude: 0.0)
-        let step = CLLocationCoordinate2D(latitude: 1.0, longitude: 1.0)
-
-        let coordinates = getCoordinatesList(start, finish, step)
-
-        if coordinates.count > 0 {
-            XCTAssertEqual(coordinates[0], start, "List starts with a proper coordinate")
-        } else {
-            XCTFail("No coordinates generated")
-        }
-    }
-}
-
-let testSuite = CoordinateGeneratorTests.defaultTestSuite
-testSuite.run()
